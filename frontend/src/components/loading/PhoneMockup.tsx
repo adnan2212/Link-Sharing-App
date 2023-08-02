b4fdec513466ea024f9f91d129754f781511b6bb
Rahul Gupta <rahul123alok@gmail.com>
2023-08-02 22:53:49 +0530

    fixed displayProfile

--- frontend/src/components/loading/PhoneMockup.tsx
+++ frontend/src/components/loading/PhoneMockup.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import useContent from "../../hooks/useContent";
+import axios from "../../api/axios";
 import { Scrollbars } from "react-custom-scrollbars"; // Import the CustomScrollbars component
 
 import githubIcon from "../../assets/icons/icon-link-boxes/icon-github-link-box.svg";
@@ -89,12 +90,35 @@ const PhoneMockup = () => {
       titleLogo: hashNodeIcon
     }
   ];
-  const { userLinks, setUserLinks } = useContent();
+  const { userLinks, auth, displayProfile, setDisplayProfile } = useContent();
+  const [isNameAvialable, setIsNameAvialable] = useState(false);
+  const token = auth.accessToken;
 
   console.log("🔗 USER's Mockup Data: ", userLinks);
   const getUserPlatformData = (platformName) => {
     return platformData.find((platform) => platform.name === platformName);
   };
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await axios.get("/profile", {
+          headers: {
+            Authorization: `Bearer ${token}`
+          },
+          withCredentials: true
+        });
+        setIsNameAvialable(true);
+        if (!response) return;
+        setDisplayProfile(response.data);
+      } catch (error) {
+        setIsNameAvialable(false);
+        console.log(error);
+      }
+    };
+    fetchUserData();
+  }, [token]);
+
   const isDarkColor = (color) => {
     // Convert color to RGB values
     const hex = color.slice(1); // Remove '#' from the color string
@@ -107,61 +131,77 @@ const PhoneMockup = () => {
     return luminance <= 0.5;
   };
 
+  const { firstName, lastName, email, profileImage } = displayProfile;
+
   return (
     <div className="flex items-center justify-center rounded-xl bg-white p-[4.25rem]">
-      <div className="h-[632px] w-[308px] overflow-hidden bg-phoneMockup bg-center bg-no-repeat pb-8 pt-60">
+      <div className="h-[632px] w-[308px] overflow-hidden bg-phoneMockup bg-center bg-no-repeat pb-8 pt-16 relative">
+        <div className="flex flex-col items-center">
+          <img
+            src={`http://localhost:3000/uploads/${profileImage}`}
+            className="mb-6 h-[104px] w-[104px] cursor-pointer rounded-full border-4 border-solid border-[#633CFF]"
+          />
+          <h1 className="mb-2 text-3xl font-bold leading-[150%] text-[#333]">
+            {isNameAvialable ? `${firstName} ${lastName}` : "Your Name"}
+          </h1>
+          <h2 className="mb-8 leading-[150%] text-[#737373]">
+            {isNameAvialable ? email : "Your Email"}
+          </h2>
+        </div>
         {/* Use Scrollbars to wrap your content */}
-        <Scrollbars
-          autoHide
-          autoHideTimeout={1000}
-          autoHideDuration={200}
-          renderThumbVertical={() => <div style={{ display: "none" }} />}
-        >
-          <div className="flex flex-col items-center gap-4">
-            {/* Your userLinks content */}
-            {userLinks.map((link) => {
-              const platform = getUserPlatformData(link.linkTitle);
-              if (platform) {
-                const dynamicStyles = {
-                  backgroundColor: platform.color,
-                  color: isDarkColor(platform.color) ? "white" : "black"
-                };
+        <div className="absolute top-[48%] left-0 bottom-0 right-0 overflow-auto pb-11 ">
+          <Scrollbars
+            autoHide
+            autoHideTimeout={1000}
+            autoHideDuration={200}
+            renderThumbVertical={() => <div style={{ display: "none" }} />}
+          >
+            <div className="flex flex-col items-center gap-4">
+              {/* Your userLinks content */}
+              {userLinks.map((link) => {
+                const platform = getUserPlatformData(link.linkTitle);
+                if (platform) {
+                  const dynamicStyles = {
+                    backgroundColor: platform.color,
+                    color: isDarkColor(platform.color) ? "white" : "black"
+                  };
 
-                return (
-                  <div
-                    key={platform.name}
-                    className="flex h-[44px] w-[237px]  justify-between rounded-xl p-1 text-center"
-                    style={dynamicStyles}
-                  >
-                    <div className="flex items-center gap-2">
-                      <img
-                        className="ml-2 inline-block w-[40%]"
-                        src={platform.titleLogo}
-                        alt={platform.name}
-                      />
-                      <h1 className="whitespace-nowrap">{platform.name}</h1>
+                  return (
+                    <div
+                      key={platform.name}
+                      className="flex h-[44px] w-[237px]  justify-between rounded-xl p-1 text-center"
+                      style={dynamicStyles}
+                    >
+                      <div className="flex items-center gap-2">
+                        <img
+                          className="ml-2 inline-block w-[40%]"
+                          src={platform.titleLogo}
+                          alt={platform.name}
+                        />
+                        <h1 className="whitespace-nowrap">{platform.name}</h1>
+                      </div>
+                      {isDarkColor ? (
+                        <img
+                          className="mr-2 inline-block w-[10%]"
+                          src={arrowRightDark}
+                          alt="Arrow"
+                        />
+                      ) : (
+                        <img
+                          className="mr-2 inline-block w-[10%]"
+                          src={arrowRight}
+                          alt="Arrow"
+                        />
+                      )}
                     </div>
-                    {isDarkColor ? (
-                      <img
-                        className="mr-2 inline-block w-[10%]"
-                        src={arrowRightDark}
-                        alt="Arrow"
-                      />
-                    ) : (
-                      <img
-                        className="mr-2 inline-block w-[10%]"
-                        src={arrowRight}
-                        alt="Arrow"
-                      />
-                    )}
-                  </div>
-                );
-              } else {
-                return null;
-              }
-            })}
-          </div>
-        </Scrollbars>
+                  );
+                } else {
+                  return null;
+                }
+              })}
+            </div>
+          </Scrollbars>
+        </div>
       </div>
     </div>
   );
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,8 +5,9 @@ export default {
     extend: {
       backgroundImage: {
         "upload-img": 'url("/src/assets/icons/con-upload-image.svg")',
-      },
-    },
+        phoneMockup: 'url("/src/assets/images/illustration-phone-mockup.svg")'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
